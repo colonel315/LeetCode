41213a197aa8b5c47d249e447ae930bfe5deda8c
Fred Burr <Fred Burr>
2017-01-02 22:13:40 -0500

    finished zigzag

--- src/Easy/ZigZag.java
+++ src/Easy/ZigZag.java
@@ -7,14 +7,32 @@ public class ZigZag {
     public static void main(String args[]) {
         ZigZag zigZag = new ZigZag();
 
-        zigZag.zigZag("WhatIsGoingOn", 5);
+        String solution = zigZag.zigZag("WhatIsGoingOn", 5);
+        System.out.print(solution);
     }
     private String zigZag(String s, int numRows) {
+        char[] chars = s.toCharArray();
+        int length = chars.length;
         StringBuilder[] builder = new StringBuilder[numRows];
-        String solution = "";
 
+        for(int j = 0; j < numRows; j++) {
+            builder[j] = new StringBuilder();
+        }
 
+        int i = 0;
+        while(i < length) {
+            for(int j = 0; j < numRows && i < length; j++) {
+                builder[j].append(chars[i++]);
+            }
+            for(int j = numRows-2; j > 0 && i < length; j--) {
+                builder[j].append(chars[i++]);
+            }
+        }
 
-        return "";
+        for(int j = 1; j < numRows; j++) {
+            builder[0].append(builder[j]);
+        }
+
+        return builder[0].toString();
     }
 }
