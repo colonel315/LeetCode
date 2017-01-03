5d64c196817475224397792d16040087aa7b04a3
Fred Burr <Fred Burr>
2017-01-02 21:49:58 -0500

    working on zigzag

---
+++ src/Easy/ZigZag.java
@@ -0,0 +1,20 @@
+package Easy;
+
+/**
+ * Created by Dell on 1/2/2017.
+ */
+public class ZigZag {
+    public static void main(String args[]) {
+        ZigZag zigZag = new ZigZag();
+
+        zigZag.zigZag("WhatIsGoingOn", 5);
+    }
+    private String zigZag(String s, int numRows) {
+        StringBuilder[] builder = new StringBuilder[numRows];
+        String solution = "";
+
+
+
+        return "";
+    }
+}
